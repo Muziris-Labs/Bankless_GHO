771c4645f98fd42fd86fdc601ada6d9264aea956
Anoy Roy Chowdhury <anoyroyc3545@gmail.com>
2024-01-19 01:50:07 +0530

    Recovery Proof Generation

--- contracts/packages/hardhat/contracts/base/RecoveryManager.sol
+++ contracts/packages/hardhat/contracts/base/RecoveryManager.sol
@@ -3,8 +3,12 @@ pragma solidity >=0.8.0 <0.9.0;
 
 import "../verifier/RecoveryVerifier.sol";
 import "../util/Conversion.sol";
+import "@openzeppelin/contracts/utils/cryptography/ECDSA.sol";
+import "@openzeppelin/contracts/utils/Strings.sol";
 
 contract RecoveryManager {
+    using ECDSA for bytes32;
+
     RecoveryUltraVerifier public recoveryVerifier;
 
     bytes32 private recoveryKeyHash;
@@ -16,13 +20,23 @@ contract RecoveryManager {
         recoveryKeyHash = _recoveryKeyHash;
     }
 
-    function getRecoveryNonce() public view returns (bytes32) {
-        return bytes32(recoveryNonce);
+    function getRecoveryNonce() public view returns (string memory) {
+        return Strings.toString(recoveryNonce);
     }
 
-    function _useRecoveryNonce() internal returns (bytes32) {
+    function _useRecoveryNonce() internal returns (string memory) {
         recoveryNonce++;
-        return bytes32(recoveryNonce);
+        return Strings.toString(recoveryNonce);
+    }
+
+    function hashMessage(string memory message) public pure returns (bytes32) {
+        string memory messagePrefix = "\x19Ethereum Signed Message:\n";
+
+        string memory lengthString = Strings.toString(bytes(message).length);
+
+        string memory concatenatedMessage = string(abi.encodePacked(messagePrefix, lengthString, message));
+
+        return keccak256(bytes(concatenatedMessage));
     }
 
     function changeRecoveryVerifier(address _recoveryVerifier) internal {
@@ -30,14 +44,14 @@ contract RecoveryManager {
     }
 
     function verifyRecovery(bytes calldata proof) public view returns (bool) {
-        bytes32 message = getRecoveryNonce();
+        bytes32 message = hashMessage(getRecoveryNonce());
 
         bytes32[] memory _inputs = Conversion.convertInputs(message, recoveryKeyHash);
 
         return recoveryVerifier.verify(proof, _inputs);
     }
 
-    function useRecovery(bytes calldata proof) public returns (bool) {
+    function useRecovery(bytes calldata proof) internal returns (bool) {
         require(verifyRecovery(proof), "Invalid recovery");
         _useRecoveryNonce();
         return true;
--- server/routes/api/utils.js
+++ server/routes/api/utils.js
@@ -84,6 +84,10 @@ router.post("/recovery/inputs", async (req, res) => {
 router.post("/passkey/generate", async (req, res) => {
   const { pubkey, signature, message } = req.body;
 
+  if (!pubkey || !signature || !message) {
+    return res.status(400).json({ msg: "Please enter all fields" });
+  }
+
   const backend = new BarretenbergBackend(passkeyCircuit);
   const noir = new Noir(passkeyCircuit, backend);
 
@@ -109,6 +113,53 @@ router.post("/passkey/generate", async (req, res) => {
     message: Array.from(messageArray),
     pub_key_x_hash: Array.from(pubkey_x_hash),
   };
+
+  const proof = await noir.generateFinalProof(input);
+
+  res.json({
+    proof: "0x" + Buffer.from(proof.proof).toString("hex"),
+  });
+});
+
+router.post("/recovery/generate", async (req, res) => {
+  const { signature, message } = req.body;
+
+  if (!signature || !message) {
+    return res.status(400).json({ msg: "Please enter all fields" });
+  }
+
+  const backend = new BarretenbergBackend(recoveryCircuit);
+  const noir = new Noir(recoveryCircuit, backend);
+
+  const pubKey_uncompressed = ethers.utils.recoverPublicKey(
+    ethers.utils.hashMessage(ethers.utils.toUtf8Bytes(message)),
+    signature
+  );
+
+  let pubKey = pubKey_uncompressed.slice(4);
+  let pub_key_x = pubKey.substring(0, 64);
+  let pub_key_y = pubKey.substring(64);
+
+  const pub_key_x_array = ethers.utils.arrayify("0x" + pub_key_x);
+  const pub_key_y_array = ethers.utils.arrayify("0x" + pub_key_y);
+
+  const signatureArray = ethers.utils.arrayify(signature);
+
+  const messageArray = ethers.utils.arrayify(
+    ethers.utils.hashMessage(ethers.utils.toUtf8Bytes(message))
+  );
+
+  const pubkey_x_hash = acvm.sha256(pub_key_x_array);
+
+  const input = {
+    pub_key_x: Array.from(pub_key_x_array),
+    pub_key_y: Array.from(pub_key_y_array),
+    signature: Array.from(signatureArray.slice(0, 64)),
+    hashed_message: Array.from(messageArray),
+    pub_key_x_hash: Array.from(pubkey_x_hash),
+  };
+
+  console.log(input);
 
   const proof = await noir.generateFinalProof(input);
 
